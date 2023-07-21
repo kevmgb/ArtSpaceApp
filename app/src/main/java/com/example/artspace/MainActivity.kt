a864cc96f3a1b4d40be12f6e73296df0b3f431f6
kevmgb <kithinjimkevin@gmail.com>
2023-07-21 19:42:03 +0300

    setup basic structure

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -3,13 +3,30 @@ package com.example.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +39,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceLayout()
                 }
             }
         }
@@ -30,17 +47,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ArtSpaceLayout() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.lemon_drink),
+            contentDescription = "Lemon Glass",
+            modifier = Modifier
+                .weight(8f)
+                .align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "Artwork Title",
+            modifier = Modifier
+//                .weight(1f)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(
+            text = "Artwork Artist (Year)",
+            modifier = Modifier
+//                .weight(1f)
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        ButtonsRow(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterHorizontally)
+        )
+
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ArtworkInformation(modifier: Modifier = Modifier) {
+        Text(text = "Artwork Title")
+        Text(text = "Artwork Artist (Year)")
+}
+
+@Composable
+fun ButtonsRow(
+    modifier: Modifier = Modifier
+) {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .padding(bottom = 10.dp)
+    ){
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(50.dp)
+                .width(150.dp)
+                .padding(end = 10.dp)
+        ) {
+            Text(text = "Previous")
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .height(50.dp)
+                .width(150.dp)
+                .padding(start = 10.dp)
+        ) {
+            Text(text = "Next")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArtSpaceLayoutPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceLayout()
     }
 }
